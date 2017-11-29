0d708f60ba3ba5ec075ff6f03085032113259f16
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-29 16:58:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,7 @@ def key_for_min_value(name_hash)
 min_key = nil
 name_hash.each do |key, value|
   if min_value == nil
-    min_value = value
-    min_key = key
+     return nil
   elsif min_value > value
     min_value = value
     min_key = key
